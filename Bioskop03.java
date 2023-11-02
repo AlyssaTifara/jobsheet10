b7b6077a0a357140f82ee8f85828f821ee1041df
Alyssa Tifara Yuwono <tissaalyssa@gmail.com>
2023-11-02 13:14:38 +0700

    soal_percobaan1

--- Bioskop03.java
+++ Bioskop03.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthLookAndFeel;
 public class Bioskop03{
     public static void main(String[] args) {
         
@@ -11,10 +13,13 @@ public class Bioskop03{
         penonton [2][0] = "Eka";
         penonton [2][1] = "Farhan";
         penonton [3][0] = "Gisel";
+        penonton [3][1] = "Hana";
 
-        System.out.printf("%s \t %s\n", penonton [0][0], penonton [0][1]);
-        System.out.printf("%s \t %s\n", penonton [1][0], penonton [1][1]);
-        System.out.printf("%s \t %s\n", penonton [2][0], penonton [2][1]);
-        System.out.printf("%s \t %s\n", penonton [3][0], penonton [3][1]);
+        System.out.println(penonton.length);
+
+        for (int i = 0; i < penonton.length; i++){
+            System.out.println("Panjang baris ke-" + (i+1) + " : " + penonton[i].length);
+        }
+
     }
 }
