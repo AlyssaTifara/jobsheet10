986ccb6f6a33325113dc02952832eb92ba929c45
Alyssa Tifara Yuwono <tissaalyssa@gmail.com>
2023-11-03 11:54:10 +0700

    soal_percobaan2

--- BioskopWithScanner03.java
+++ BioskopWithScanner03.java
@@ -1,31 +1,43 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class BioskopWithScanner03{
     public static void main(String[] args){
-        Scanner sc = new Scanner (System.in);
+        Scanner input1 = new Scanner (System.in);
+        Scanner input2 = new Scanner(System.in);
 
         String nama, next;
-        int baris, kolom;
+        int baris, kolom,menu;
+        boolean exit;
 
         String [][] penonton = new String[4][2];
 
         while (true){
-            System.out.print("Masukkan nama: ");
-            nama = sc.nextLine();
-            System.out.print("Masukkan baris: ");
-            baris = sc.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom = sc.nextInt();
-            sc.nextLine();
+            System.out.print("Menu:");
+            System.out.print("\n1. Input data penonton");
+            System.out.print("\n2. Menampilkan data penonton");
+            System.out.print("\n3. Exit");
+            System.out.print("\nPilih menu (1/2/3): ");
+            // input.nextInt(); 
 
-            penonton[baris-1][kolom-1] = nama;
-
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = sc.nextLine();
+            int pilihan = input1.nextInt();
             
-            if (next.equalsIgnoreCase("n")){
-                break;
+            
+            if (pilihan == 1){
+            System.out.println("Masukkan nama penonton");
+            String namaPenonton = input2.nextLine();
+            System.out.println("Pilih baris tempat duduk yang anda inginkan ");
+            baris = input1.nextInt();
+            System.out.println("Kolom : ");
+            kolom = input1.nextInt();
+            penonton [baris-1] [kolom-1] = namaPenonton;
+        } else if (pilihan == 2){
+            for (int i = 0; i < penonton.length; i++){
+                System.out.println("Pada baris ke- " +(i+1) + " : "+penonton[i][0] +", "+penonton[i][1]);
             }
-
+        } else if (pilihan == 3){
+            System.out.println("Terimakasih telah memilih jasa kami");
+            break;
+        }
         }
     }
 }
